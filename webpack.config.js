9723adadeaf5e126190ce2689b718df5cb0d91df
Marc d'Entremont <marc.dentremont@bluescape.com>
2020-10-23 19:25:00 -0700

    update deps and add context folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   entry: {
-    'jsonld-patch': './lib'
+    'jsonld-patch': './lib',
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'contexts/*.json',
+          context: 'lib/'
+        },
+      ],
+    }),
+  ],
   output: {
     filename: '[name].min.js',
     library: '[name]',
     libraryTarget: 'amd',
-  }
-}
+  },
+};
